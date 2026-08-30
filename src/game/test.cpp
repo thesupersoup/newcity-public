66820b36d507059ce9816c3fd16264024a4d9ce0
thesupersoup <40295603+thesupersoup@users.noreply.github.com>
2026-08-30 14:12:17 -0500

    fix: uncomment catch config main def in test cpp so game_test can build

--- src/game/test.cpp
+++ src/game/test.cpp
@@ -1,6 +1,6 @@
 // This tells Catch to provide a main() - only do this in one cpp file
-//#define CATCH_CONFIG_MAIN
-//#include "catch.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 /*
 unsigned int Factorial( unsigned int number ) {
