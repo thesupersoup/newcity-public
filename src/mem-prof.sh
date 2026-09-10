5de34283b6571a907f1a2f4e6f76394422c0892a
thesupersoup <40295603+thesupersoup@users.noreply.github.com>
2026-09-09 23:09:01 -0500

    chore: remove steam build, bin, scripts, and refs

--- src/mem-prof.sh
+++ src/mem-prof.sh
@@ -2,7 +2,7 @@
 
 #mkdir -p ../build
 #cd ../build && make -j 24 newcity && cd ../src && \
-export LD_PRELOAD="./libsteam_api.so ./libOpenGL.so.0"; \
+export LD_PRELOAD="./libOpenGL.so.0"; \
   valgrind --tool=massif --xtree-memory=full -v ./newcity
 
   #--cache-sim=yes --branch-sim=yes --collect-bus=yes --dump-instr=yes \
